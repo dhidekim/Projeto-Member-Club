487a2ac080eec1f35bfc6464de34891d86e7919f
Douglas <dhidekim@gmail.com>
2024-09-14 08:12:31 -0300

    Show dados do cliente

--- src/modules/member/show.js
+++ src/modules/member/show.js
@@ -1,6 +1,87 @@
 export function memberShow({ dataMember }) {
   try {
     console.log(dataMember);
+    const client = dataMember[0];
+
+    const dataClient = document.querySelector("#data-client");
+    const dataPhoto = document.querySelector("#data-photo");
+    const clientId = document.querySelector("#client-id");
+    const appointmentHistoryList = document.querySelector(
+      "#appointment-history ul"
+    );
+    const totalCuts = document.getElementById("total-cuts");
+    const cutsRemaining = document.querySelector(".cuts-remaining h2 span");
+    const count = document.querySelector(".cuts-remaining .count");
+    const progressText = document.querySelector(".progress span");
+    const progressBar = document.querySelector(".progress-bar");
+    const loyaltyCardList = document.querySelector(".loyalty-card ul");
+
+    const photo = document.createElement("img");
+    const name = document.createElement("h1");
+    const clientSince = document.createElement("p");
+
+    const clientCutsNeeded = client.loyaltyCard.cutsNeeded;
+    const clientTotalCuts = client.loyaltyCard.totalCuts;
+
+    // Limpa
+    dataClient.innerHTML = "";
+    dataPhoto.innerHTML = "";
+    appointmentHistoryList.innerHTML = "";
+    loyaltyCardList.innerHTML = "";
+    //Dados do Cliente
+    photo.setAttribute("src", `./src/assets/${client.id}.png`);
+    photo.setAttribute("alt", client.name);
+    name.textContent = client.name;
+    clientSince.textContent = `Cliente desde ${client.clientSince}`;
+    clientId.textContent = `ID: ${client.id}`;
+
+    dataPhoto.append(photo);
+    dataClient.append(name, clientSince);
+
+    //Histórico de corte
+    client.appointmentHistory.forEach((appointment) => {
+      const item = document.createElement("li");
+      const date = document.createElement("strong");
+      const time = document.createElement("span");
+
+      date.textContent = appointment.date;
+      time.textContent = appointment.time;
+
+      item.append(date, time);
+      appointmentHistoryList.appendChild(item);
+    });
+
+    //Total de cortes
+    totalCuts.textContent = `${clientTotalCuts} cortes`;
+
+    //Cortes restantes
+    cutsRemaining.textContent = client.loyaltyCard.cutsRemaining;
+
+    //corte restante de um total de
+    count.textContent = `${clientTotalCuts} de ${clientCutsNeeded}`;
+
+    //Barra de progresso
+    progressBar.style.width = `${clientTotalCuts * 10}%`;
+    progressBar.setAttribute("aria-valuenow", `${clientTotalCuts * 10}`);
+    progressText.textContent = `${clientTotalCuts * 10}% Completo`;
+
+    //Cartão Fidelidade
+    for (let i = 0; i < clientCutsNeeded; i++) {
+      const item = document.createElement("li");
+
+      if (i < clientTotalCuts) {
+        item.classList.add("done");
+        item.setAttribute("title", "OK");
+        item.innerHTML = "<span>Ok</span>";
+      } else if (i == clientCutsNeeded - 1) {
+        item.classList.add("free");
+        item.innerHTML = "<span>Ganhou</span>";
+      } else {
+        item.innerHTML = "<span>Vazio</span>";
+      }
+
+      loyaltyCardList.appendChild(item);
+    }
   } catch (error) {
     alert("Não foi possível exibir os dados do membro");
     console.error(error);
